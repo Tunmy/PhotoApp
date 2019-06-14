0e5a34115680dbb8f4112b4c54fcde2b9760540f
Grace Owolabi <gowolabi2@gmail.com>
2019-06-14 20:55:02 +0000

    Added registration Controller

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,8 +12,8 @@ class Payment < ApplicationRecord
     
     def process_payment
         customer = Stripe::Customer.create email: email, card: token
-        
-        Stripe::Charge.create customer: customer_id, amount: 1000,
+        Stripe::Charge.create customer: customer.id,
+                                        amount: 1000,
                                         description: 'Premium',
                                         currency: 'usd'
     end
